686419a11293b85e8188ce8e8bd9a1a42c7a08f3
Valerio <valerio.cavagni@gmail.com>
2021-04-11 23:32:27 +0200

    Update for new IT lang

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -13,6 +13,7 @@ server_scripts {
 	'locales/fr.lua',
 	'locales/sv.lua',
 	'locales/pl.lua',
+	'locales/it.lua',
 	'config.lua',
 	'server/main.lua'
 }
@@ -25,6 +26,7 @@ client_scripts {
 	'locales/fr.lua',
 	'locales/sv.lua',
 	'locales/pl.lua',
+	'locales/it.lua',
 	'config.lua',
 	'client/main.lua'
 }
