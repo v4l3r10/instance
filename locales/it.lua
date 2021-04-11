42f9aea175198e42a908e79df4b4ab02ddac58f5
Valerio <valerio.cavagni@gmail.com>
2021-04-11 23:33:06 +0200

    Fix, typo

--- locales/it.lua
+++ locales/it.lua
@@ -1,6 +1,6 @@
 Locales['it'] = {
   ['left_instance'] = 'hai lasciato l\'edificio',
-  ['invite_expired'] = 'l\invito è scaduto',
+  ['invite_expired'] = 'l\'invito è scaduto',
   ['press_to_enter'] = 'premi ~INPUT_CONTEXT~ per entrare nell\'edificio',
   ['entered_instance'] = 'sei entrato nell\'edificio',
   ['entered_into'] = '%s è entrato nell\'edificio',
