17a5dae2336e09ed67cb3427bd5aca1e010ab33d
Valerio <valerio.cavagni@gmail.com>
2021-04-11 23:32:01 +0200

    [Add] IT translation

---
+++ locales/it.lua
@@ -0,0 +1,8 @@
+Locales['it'] = {
+  ['left_instance'] = 'hai lasciato l'edificio',
+  ['invite_expired'] = 'l\invito è scaduto',
+  ['press_to_enter'] = 'premi ~INPUT_CONTEXT~ per entrare nell\'edificio',
+  ['entered_instance'] = 'sei entrato nell\'edificio',
+  ['entered_into'] = '%s è entrato nell\'edificio',
+  ['left_out'] = '%s è uscito dall\'edificio',
+}
